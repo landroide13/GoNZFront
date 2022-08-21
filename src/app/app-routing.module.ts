a0ebb9af82ad15770d8e63ce21ddbf9afaaff0c0
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-08-22 11:57:23 +1200

    Some Stylling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AgentListComponent } from './agent-list/agent-list.component';
 import { AuthComponent } from './auth/auth.component';
+import { MainComponent } from './main/main.component';
 import { TourListComponent } from './tour-list/tour-list.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   {path:'auth', component:AuthComponent},
   {path:'agents', component:AgentListComponent},
   {path:'tours', component:TourListComponent},
+  {path:'home', component:MainComponent},
 ];
 
 @NgModule({
