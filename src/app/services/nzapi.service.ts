7f60682ac0eb9c082185308bd50539eb50264778
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-08-22 12:29:29 +1200

    service and style

--- src/app/services/nzapi.service.ts
+++ src/app/services/nzapi.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NzapiService {
 
-  constructor() { }
+  url = 'http://localhost:8000/';
+
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+
+  constructor(private  http: HttpClient) { }
+
+
+  getAgents(){
+
+  }
+
+  getTours(){
+
+  }
+
+  getAgent(id: any){
+
+  }
+
+  getTour(id: any){
+    
+  }
+
+
+
 }
